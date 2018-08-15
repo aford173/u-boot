ff93101d598f73baaf5b5a29eb2922d4830c93fe
Adam Ford <aford173@gmail.com>
2018-08-15 11:09:41 -0500

    ARM: da850evm: Enable DM in SPL
    With the Davinci SPI driver fully migrated to DM, this patch
    enables DM in SPL and uses OF_PLATDATA in SPL to save space
    with limited SPL resources.
    
    Signed-off-by: Adam Ford <aford173@gmail.com>

--- include/configs/da850evm.h
+++ include/configs/da850evm.h
@@ -20,16 +20,6 @@
 #endif
 
 /*
-* Disable DM_* for SPL build and can be re-enabled after adding
-* DM support in SPL
-*/
-#ifdef CONFIG_SPL_BUILD
-#undef CONFIG_DM_SPI
-#undef CONFIG_DM_SPI_FLASH
-#undef CONFIG_DM_I2C
-#undef CONFIG_DM_I2C_COMPAT
-#endif
-/*
  * SoC Configuration
  */
 #define CONFIG_SYS_EXCEPTION_VECTORS_HIGH
