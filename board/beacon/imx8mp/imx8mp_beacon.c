fe2a75e80efd053c38241be91c00aa64ae6f0b5c
Adam Ford <aford173@gmail.com>
2025-04-06 18:06:54 -0500

    arm64: imx: Add TYPEC power support
    The i.MX8MP uses a USB Type C connector to provide power, but
    it doesn't actually use it for data.  Enable the driver and
    configure it to supply 9V.
    
    TCPC:  Vendor ID [0x1fc9], Product ID [0x5110], Addr [I2C2 0x53]
    SNK.Power3.0 on CC1
    PDO 0: type 0, 5000 mV, 3000 mA [E]
    PDO 1: type 0, 9000 mV, 2000 mA []

--- board/beacon/imx8mp/imx8mp_beacon.c
+++ board/beacon/imx8mp/imx8mp_beacon.c
@@ -5,6 +5,7 @@
 #include <init.h>
 #include <miiphy.h>
 #include <asm/arch/sys_proto.h>
+#include "../../freescale/common/tcpc.h"
 
 static void setup_fec(void)
 {
@@ -24,9 +25,47 @@ int board_phy_config(struct phy_device *phydev)
 }
 #endif
 
+#ifdef CONFIG_USB_TCPC
+
+/* Configure the incoming Power */
+static int setup_typec(void)
+{
+	const struct tcpc_port_config power_port_config = {
+		.i2c_bus = 2, /*i2c3*/
+		.addr = 0x53,
+		.port_type = TYPEC_PORT_UFP,
+		.max_snk_mv = 9000,
+		.max_snk_ma = 3000,
+		.max_snk_mw = 45000,
+		.op_snk_mv = 27000,
+	};
+	int ret;
+	struct tcpc_port power_port;
+
+	debug("tcpc_init power port\n");
+	ret = tcpc_init(&power_port, power_port_config, NULL);
+
+	if (ret) {
+		printf("%s: tcpc power power init failed, err=%d\n",
+		       __func__, ret);
+		return ret;
+	}
+	return 0;
+}
+
+#endif /* CONFIG_USB_TCPC */
+
 int board_init(void)
 {
 	int ret = 0;
+
+#ifdef CONFIG_USB_TCPC
+	setup_typec();
+
+	/* Enable USB power default */
+	imx8m_usb_power(0, true);
+	imx8m_usb_power(1, true);
+#endif
 
 	if (CONFIG_IS_ENABLED(FEC_MXC))
 		setup_fec();
